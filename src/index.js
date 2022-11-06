90f557c6b483c9cd8fae07c28560879372f34364
finleypeng <weilllyox1020@gmail.com>
2022-11-06 15:11:12 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components'
+import App from './components/App'
+
+const theme = {
+	color: {
+		red_300: '#fa4848',
+		red_400: '#440000',
+		red_500: '#660000',
+	}
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<ThemeProvider theme={theme}>
+		<App />
+	</ThemeProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
